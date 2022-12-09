a2003d36ce8d2d183fde8f9306ae14c0e9f93df2
I.K <63575645+Tellschuss@users.noreply.github.com>
2022-12-09 17:10:17 +0600

    Add files via upload

---
+++ plugins/vuetify.ts
@@ -0,0 +1,15 @@
+import { createVuetify, ThemeDefinition } from "vuetify";
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin(nuxtApp =>{
+const vuetify = createVuetify({
+components,
+directives
+theme: {
+    defaultTheme: 'light'
+}
+})
+
+nuxtApp.vueApp.use(vuetify)
+})
